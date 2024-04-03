ba962c67ba66005d5b1c24e03d37f0321f94c627
DavitGogiava <ursamajor027@gmail.com>
2024-04-03 03:40:27 -0700

    new

--- src/Components/AutoImportPage/CardSlider.jsx
+++ src/Components/AutoImportPage/CardSlider.jsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Rating } from "react-simple-star-rating";
 
 export default function CardSlider() {
